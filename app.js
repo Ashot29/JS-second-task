dc4527ecbdb3bfe509cc5a5fa138b059db661c2c
Ashot Sargsyan <sargsyan.ashot19@gmail.com>
2021-05-16 16:02:45 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+// // Page 42
+// // Task 12
+
+// let a = +prompt('enter a number', 'a');
+// let i = 1;
+// let s = 0;
+
+// if (a >= 0) {
+//     while (i <= a) {
+//         s = s + i**2;
+//         i = i + 1; 
+//     }
+// }
+
+// console.log(s)
+
+
+// // Task 13
+
+// let a = +prompt('Enter a number', 'a');
+// let y = 1;
+// let i = 0;
+
+// while ( i < a ) {
+//     y = y * 2;
+//     i = i + 1;
+// }
+
+// console.log(y)
+
+
+// // Task 14
+
+// let a = +prompt('Enter a big number');
+// let i = 1;
+// let s = 0;
+
+// while (s < a) {
+//     s = s + i**2;
+//     i++;
+// }
+
+// console.log(s)
+
+
+// // Task 15
+
+// let a = +prompt('Enter a number', 'a');
+
+// let s = 0;
+// let i = 1;
+// let z = 2;
+
+// while (i <= a) {
+//     s = s + z;
+//     i = i + 1;
+//     z = z * 2;
+// } 
+
+// console.log(s)
+
+
+// // Task 16
+
+// let n = +prompt('Enter a number', 'n');
+
+// let s = 0;
+// let y = 0;
+
+// for (let i = 1; i < n; i++) {
+//     y = y + Math.sin(i);
+//     s = s + (i / y)
+// }
+
+// console.log(s)
+
+
+// // Task 21
+
+// let x = +prompt('Enter a number', 'x');
+// let e = +prompt('Enter a number', 'e');
+
+// let s = 0;
+// let n = 1;
+// let u = x;
+
+// while(u > e) {
+//     s = s + u;
+//     u = u * (x**2) / (2*n) * (2*n + 1);
+//     n = n + 1;
+// }
+
+// console.log(s)
